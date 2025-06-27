1bf410158704ef970f5dfa363ef6b9bac1c9874f
vike <vikesun@vikedeMacBook-Pro.local>
2025-06-28 01:29:24 +0800

    fix: script

--- script/wAbel.s.sol
+++ script/wAbel.s.sol
@@ -8,7 +8,15 @@ contract wAbelScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        // 检查私钥是否存在
+        if (vm.envOr("PRIVATE_KEY", uint256(0)) == 0) {
+            revert("PRIVATE_KEY not set in .env");
+        }
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        console2.log("Deployer address:", deployer);
+        
+        vm.startBroadcast(deployerPrivateKey);
         wAbel token = new wAbel();
         console2.log("wAbel deployed at:", address(token));
         vm.stopBroadcast();
